a07e8ff2d3a4422c429cceb9f0259e669b080e12
Ignacio Dominguez Martinez-Casanueva <i.dominguezm@alumnos.upm.es>
2020-10-16 11:58:54 +0200

    Merge query-collector and NGSI-LD fastapi

--- collector/query-collector/app/main.py
+++ collector/query-collector/app/main.py
@@ -1,61 +1,38 @@
-from prometheus_api_client import PrometheusConnect, MetricSnapshotDataFrame, MetricRangeDataFrame
-from starlette.applications import Starlette
-from starlette.responses import PlainTextResponse
-from starlette.routing import Route
+from fastapi import FastAPI, status
+from prometheus_api_client import PrometheusConnect, MetricSnapshotDataFrame
+from typing import Optional
 
 import datetime as dt
 
+# Prometheus Config
+prom = PrometheusConnect(url="http://prometheus:9099", disable_ssl=True)
 
-def parse_labels(params):
-    labels = {}
-    for param, value in params.multi_items():
-        if param == 'metric_name' or param == 'range':
-            continue
-        labels[param] = value
-    return labels
+# FastAPI specific code
+tags_metadata = [
+    {
+        "name": "Context Information",
+        "description": "NGSI-LD Entities",
+    }
+]
+
+app = FastAPI(
+    title="NGSI-LD API",
+    version="1.0.0",
+    openapi_tags=tags_metadata)
 
 
-async def metrics(request):
-    metric_name = request.query_params['metric_name']
-    labels = parse_labels(request.query_params)
-
-    prom = PrometheusConnect(url="http://prometheus:9090", disable_ssl=True)
+@app.get("/ngsi-ld/v1/entities/",
+         summary="Query entities",
+         description="Retrieve a set of entities which matches \
+                      a specific query from an NGSI-LD system",
+         tags=["Context Information"],
+         status_code=status.HTTP_200_OK)
+async def queryEntities(type: str, attrs: Optional[str] = None,
+                        q: Optional[str] = None):
     # metric current values
     metric_data = prom.get_current_metric_value(
-        metric_name=metric_name,
-        label_config=labels
-    )
+        metric_name="prometheus_http_requests_total",
+        label_config=labels)
     metric_df = MetricSnapshotDataFrame(metric_data)
     metric_df.pop('__name__')
     print(metric_df.to_string())
-
-    return PlainTextResponse()
-
-
-async def metrics_range(request):
-    metric_name = request.query_params['metric_name']
-    range = request.query_params['range']
-    labels = parse_labels(request.query_params)
-
-    prom = PrometheusConnect(url="http://prometheus:9090", disable_ssl=True)
-    # metric current values
-    metric_data = prom.get_metric_range_data(
-        metric_name=metric_name,
-        label_config=labels,
-        start_time=(dt.datetime.now() - dt.timedelta(minutes=int(range))),
-        end_time=dt.datetime.now()
-    )
-    if metric_data:
-        metric_df = MetricRangeDataFrame(metric_data)
-        metric_df.pop('__name__')
-        print(metric_df.to_string())
-
-    return PlainTextResponse()
-
-
-routes = [
-    Route("/metrics", endpoint=metrics, methods=["GET"]),
-    Route("/metrics_range", endpoint=metrics_range, methods=["GET"])
-]
-
-app = Starlette(routes=routes)
