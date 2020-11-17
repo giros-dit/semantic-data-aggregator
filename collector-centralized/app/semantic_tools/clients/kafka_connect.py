e535848d7cc978a4edc5de79426c8f851802a9d1
daniel-gonzalez-sanchez <daniel.gonzalez.sanchez@alumnos.upm.es>
2020-11-17 17:17:33 +0100

    Update Kafka API REST client

--- collector-centralized/app/semantic_tools/clients/kafka_connect.py
+++ collector-centralized/app/semantic_tools/clients/kafka_connect.py
@@ -21,12 +21,9 @@ class kafkaConnectClient():
         self.debug = debug
         if self.debug:
             import logging
-            # These two lines enable debugging at httplib level
-            # (requests->urllib3->http.client)
-            # You will see the REQUEST, including HEADERS and DATA,
-            # and RESPONSE with HEADERS but without DATA.
-            # The only thing missing will be
-            # the response.body which is not logged.
+            # These two lines enable debugging at httplib level (requests->urllib3->http.client)
+            # You will see the REQUEST, including HEADERS and DATA, and RESPONSE with HEADERS but without DATA.
+            # The only thing missing will be the response.body which is not logged.
             try:
                 import http.client as http_client
             except ImportError:
@@ -34,25 +31,34 @@ class kafkaConnectClient():
                 import httplib as http_client
             http_client.HTTPConnection.debuglevel = 1
 
-            # You must initialize logging,
-            # otherwise you'll not see debug output.
+            # You must initialize logging, otherwise you'll not see debug output.
             logging.basicConfig()
             logging.getLogger().setLevel(logging.DEBUG)
             requests_log = logging.getLogger("requests.packages.urllib3")
             requests_log.propagate = True
 
-    # Connect Cluster
-    def getCluster(self):
+    # Get API Connect information
+    def getAPIConnect(self):
         response = self._session.get("{0}/".format(self.url),
                                      verify=self.ssl_verification,
                                      headers=self.headers)
         if response.status_code == 200:
             return response.json()
         else:
-            response.raise_for_status()
+            return response.raise_for_status()
 
-    # Connectors
-    def createConnector(self, name: str, config: dict):
+    # Get Connectors Plugins information
+    def getConnectorsPlugins(self):
+        response = self._session.get("{0}/connector-plugins".format(self.url),
+                                     verify=self.ssl_verification,
+                                     headers=self.headers)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            return response.raise_for_status()
+
+    # Create a Kafka Connector
+    def createConnector(self, config: dict):
         response = self._session.post(
             "{0}/connectors".format(self.url),
             verify=self.ssl_verification,
@@ -62,8 +68,9 @@ class kafkaConnectClient():
         if response.status_code == 200:
             return response.json()
         else:
-            response.raise_for_status()
+            return response.raise_for_status()
 
+    # Delete a Kafka Connector by name
     def deleteConnector(self, name: str):
         response = self._session.delete(
             "{0}/connectors/{1}".format(self.url, name),
@@ -73,8 +80,19 @@ class kafkaConnectClient():
         if response.status_code == 200:
             return response.json()
         else:
-            response.raise_for_status()
+            return response.raise_for_status()
 
+    # Obtain all the Kafka Connectors
+    def getConnectors(self):
+        response = self._session.get("{0}/connectors".format(self.url),
+                                     verify=self.ssl_verification,
+                                     headers=self.headers)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            return response.raise_for_status()
+
+    # Obtain a Kafka Connector by name
     def getConnector(self, name: str):
         response = self._session.get(
             "{0}/connectors/{1}".format(self.url, name),
@@ -84,19 +102,34 @@ class kafkaConnectClient():
         if response.status_code == 200:
             return response.json()
         else:
-            response.raise_for_status()
+            return response.raise_for_status()
 
-    def getConnectors(self):
-        response = self._session.get("{0}/connectors".format(self.url),
-                                     verify=self.ssl_verification,
-                                     headers=self.headers)
+    # Obtain a Kafka Connector configuration
+    def getConnectorConfig(self, name: str):
+        response = self._session.get(
+            "{0}/connectors/{1}/config".format(self.url, name),
+            verify=self.ssl_verification,
+            headers=self.headers
+        )
         if response.status_code == 200:
             return response.json()
         else:
-            response.raise_for_status()
+            return response.raise_for_status()
 
-    # Topics
-    def getTopics(self, name: str):
+    # Obtain a Kafka Connector status
+    def getConnectorStatus(self, name: str):
+        response = self._session.get(
+            "{0}/connectors/{1}/status".format(self.url, name),
+            verify=self.ssl_verification,
+            headers=self.headers
+        )
+        if response.status_code == 200:
+            return response.json()
+        else:
+            return response.raise_for_status()
+
+    # Obtain a Kafka Connector topics
+    def getConnectorTopics(self, name: str):
         response = self._session.get(
             "{0}/connectors/{1}/topics".format(self.url, name),
             verify=self.ssl_verification,
@@ -105,4 +138,4 @@ class kafkaConnectClient():
         if response.status_code == 200:
             return response.json()
         else:
-            response.raise_for_status()
+            return response.raise_for_status()
