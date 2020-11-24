e725414826a1e3ca533c977dee2691ce372ed515
daniel-gonzalez-sanchez <daniel.gonzalez.sanchez@alumnos.upm.es>
2020-11-24 20:41:13 +0100

    Fix delete_kafkaconnectors.py Python script errors

--- collector-centralized/app/delete_kafkaconnectors.py
+++ collector-centralized/app/delete_kafkaconnectors.py
@@ -1,13 +1,9 @@
 from semantic_tools.clients.ngsi_ld import ngsildClient
 from semantic_tools.clients.kafka_connect import kafkaConnectClient
 from semantic_tools.models.prometheus_entities import MetricSource
-<<<<<<< HEAD
 from kafka import KafkaClient, KafkaAdminClient, KafkaConsumer
 from kafka.errors import KafkaError
 import time
-=======
-from kafka import KafkaAdminClient
->>>>>>> b7e0e723ca95e12f30342bef46415c83f42dbf05
 
 # Init NGSI-LD API Client
 ngsi = ngsildClient(url="http://scorpio:9090", headers={"Accept": "application/ld+json"}, context="http://context-catalog:8080/prometheus-context.jsonld")
@@ -18,7 +14,6 @@ kafka_connect_client = kafkaConnectClient(url="http://kafka-connect:8083")
 # Init Kafka Admin API Client
 kafka_admin_client = KafkaAdminClient(bootstrap_servers="kafka:9092")
 
-<<<<<<< HEAD
 # Init Kafka Consumer API to get topics
 kafka_consumer = KafkaConsumer(bootstrap_servers="kafka:9092")
 
@@ -75,19 +70,4 @@ except KafkaError:
 total_topics = kafka_consumer.topics()
 print("Total Kafka topics after Kafka Connector topics deletion:", total_topics)
 print("")
-=======
-# Query MetricSource NGSI-LD Entities
-metricsource_entities = ngsi.queryEntities(type="MetricSource")
-
-# A list of topic names to delete
-topics = []
-
-for i in range(0, len(metricsource_entities)):
-    metricsource_entity=ngsi.retrieveEntityById(metricsource_entities[i]['id'])
-    metricsource=MetricSource.parse_obj(metricsource_entity)
-    topics.append(metricsource.topic.value)
-    kafka_connect_client.deleteConnector("prometheus-source-"+str(i+1))
-
-kafka_admin_client.delete_topics(topics)
->>>>>>> b7e0e723ca95e12f30342bef46415c83f42dbf05
 
