febbe9dcee1cde870626e542e8165407184bb9b8
Ignacio Dominguez Martinez-Casanueva <i.dominguezm@alumnos.upm.es>
2020-12-10 10:55:50 +0100

    [S-10] Refactor code and rename collector to weaver

---
+++ consumer/app/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI, status, Request
+
+# FastAPI specific code
+tags_metadata = [
+    {
+        "name": "Dummy Consumer"
+    }
+]
+
+app = FastAPI(
+    title="Dummy Consumer API",
+    version="1.0.0",
+    openapi_tags=tags_metadata)
+
+# API for consumer
+@app.post("/notify",
+          status_code=status.HTTP_200_OK)
+async def consumerHello(request: Request):
+    print(await request.json())
