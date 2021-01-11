1433f08a25601a29aac96847edbd8218cdaa05f9
daniel-gonzalez-sanchez <daniel.gonzalez.sanchez@alumnos.upm.es>
2021-01-11 15:31:10 +0100

    Updating kafka version and fixing node-exporter jobs in prometheus configuration file

--- kafka/connect/config/start-kafka.sh
+++ kafka/connect/config/start-kafka.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 
 # connector start command here.
-exec "/opt/kafka_2.13-2.6.0/bin/connect-distributed.sh" "/opt/kafka_2.13-2.6.0/config/connect-distributed.properties"
+exec "/opt/kafka_2.13-2.7.0/bin/connect-distributed.sh" "/opt/kafka_2.13-2.7.0/config/connect-distributed.properties"
