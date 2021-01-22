3715cbfacbcb2ec218b8be4bd5a1613babe33a43
Ignacio Dominguez Martinez-Casanueva <i.dominguezm@alumnos.upm.es>
2021-01-22 17:24:27 +0100

    [S-41] Add base MetricStage model

--- weaver/app/semantic_tools/models/metric.py
+++ weaver/app/semantic_tools/models/metric.py
@@ -1,13 +1,29 @@
 from .ngsi_ld.entity import Entity, Property, Relationship
+from enum import Enum
 from pydantic import AnyUrl
 from typing import Literal, Optional
+
 
 class _URI(Property):
     value: AnyUrl
 
+
+class ModeResult(Enum):
+    in_progress = "IN_PROGRESS"
+    successful = "SUCCESSFUL"
+    failed = "FAILED"
+
+
+class StageMode(Enum):
+    start = "START"
+    stop = "STOP"
+    failed = "TERMINATE"
+
+
 class Credentials(Entity):
     type: Literal["Credentials"] = "Credentials"
     authMethod: Property
+
 
 class Endpoint(Entity):
     type: Literal["Endpoint"] = "Endpoint"
@@ -15,25 +31,38 @@ class Endpoint(Entity):
     name: Property
     uri: _URI
 
-class MetricSource(Entity):
+
+class ModeResultProperty(Property):
+    modeInfo: Optional[Property]
+
+
+class MetricStage(Entity):
+    type: Literal["MetricStage"] = "MetricStage"
+    stageMode: Property
+    modeResult: ModeResultProperty
+
+
+class MetricSource(MetricStage):
     type: Literal["MetricSource"] = "MetricSource"
     expression: Optional[Property] = None
     hasEndpoint: Relationship
     interval: Property
     name: Property
 
-class MetricTarget(Entity):
-    type: Literal["MetricTarget"] = "MetricTarget"
-    hasInput: Relationship
-    uri: _URI
 
-class MetricProcessor(Entity):
+class MetricProcessor(MetricStage):
     type: Literal["MetricProcessor"] = "MetricProcessor"
     hasInput: Relationship
     hasApplication: Relationship
     name: Property
     arguments: Optional[Property] = None
-    jobId: Property
+
+
+class MetricTarget(MetricStage):
+    type: Literal["MetricTarget"] = "MetricTarget"
+    hasInput: Relationship
+    uri: _URI
+
 
 class StreamApplication(Entity):
     type: Literal["StreamApplication"] = "StreamApplication"
