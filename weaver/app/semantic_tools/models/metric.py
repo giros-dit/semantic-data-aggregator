001f6050f47d9a1782a04177622224037ebb3050
daniel-gonzalez-sanchez <daniel.gonzalez.sanchez@alumnos.upm.es>
2021-01-25 15:29:46 +0100

    [S-25] Fix MetricProcessor and StreamApplication entities definition and updata context-catalog

--- weaver/app/semantic_tools/models/metric.py
+++ weaver/app/semantic_tools/models/metric.py
@@ -2,15 +2,12 @@ from .ngsi_ld.entity import Entity, Property, Relationship
 from pydantic import AnyUrl
 from typing import Literal, Optional
 
-
 class _URI(Property):
     value: AnyUrl
-
 
 class Credentials(Entity):
     type: Literal["Credentials"] = "Credentials"
     authMethod: Property
-
 
 class Endpoint(Entity):
     type: Literal["Endpoint"] = "Endpoint"
@@ -18,6 +15,18 @@ class Endpoint(Entity):
     name: Property
     uri: _URI
 
+class MetricSource(Entity):
+    type: Literal["MetricSource"] = "MetricSource"
+    expression: Optional[Property] = None
+    hasEndpoint: Relationship
+    interval: Property
+    name: Property
+    TSDB: Property
+
+class MetricTarget(Entity):
+    type: Literal["MetricTarget"] = "MetricTarget"
+    hasInput: Relationship
+    uri: _URI
 
 class MetricProcessor(Entity):
     type: Literal["MetricProcessor"] = "MetricProcessor"
@@ -25,20 +34,7 @@ class MetricProcessor(Entity):
     hasApplication: Relationship
     name: Property
     arguments: Optional[Property] = None
-
-
-class MetricSource(Entity):
-    type: Literal["MetricSource"] = "MetricSource"
-    expression: Optional[Property] = None
-    hasEndpoint: Relationship
-    interval: Property
-    name: Property
-
-
-class MetricTarget(Entity):
-    type: Literal["MetricTarget"] = "MetricTarget"
-    hasInput: Relationship
-    uri: _URI
+    jobId: Property
 
 class StreamApplication(Entity):
     type: Literal["StreamApplication"] = "StreamApplication"
