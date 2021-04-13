be465c25ad16a2a9310065fb0896ff2ddceea17d
daniel-gonzalez-sanchez <daniel.gonzalez.sanchez@alumnos.upm.es>
2021-04-13 16:23:32 +0200

    [S-80] Updating startup configuration scripts for Arista cEOS router containers

--- ceos-startup.sh
+++ ceos-startup.sh
@@ -1,7 +1,17 @@
-echo "Uploading ceos1 Arista router startup configuration..."
-docker cp ./ceos-arista/ceos-startup-config/startup-config-ceos1 ceos1:/mnt/flash/startup-config
+echo "ceos1 Arista cEOS router startup configuration:"
+echo "Uploading gNMI management protocol configuration..."
+docker cp ./ceos-arista/ceos-startup-config/gnmi-config-ceos1 ceos1:/mnt/flash/startup-config
+docker exec -it ceos1 Cli -p 15 -c "copy start run"
+echo "Uploading addressing configuration..."
+docker cp ./ceos-arista/ceos-startup-config/addressing-config-ceos1 ceos1:/mnt/flash/startup-config
 docker exec -it ceos1 Cli -p 15 -c "copy start run"
 
-echo "Uploading ceos2 Arista router startup configuration..."
-docker cp ./ceos-arista/ceos-startup-config/startup-config-ceos2 ceos2:/mnt/flash/startup-config
+echo ""
+
+echo "ceos2 Arista cEOS router startup configuration:"
+echo "Uploading gNMI management protocol configuration..."
+docker cp ./ceos-arista/ceos-startup-config/gnmi-config-ceos2 ceos2:/mnt/flash/startup-config
+docker exec -it ceos2 Cli -p 15 -c "copy start run"
+echo "Uploading addressing configuration..."
+docker cp ./ceos-arista/ceos-startup-config/addressing-config-ceos2 ceos2:/mnt/flash/startup-config
 docker exec -it ceos2 Cli -p 15 -c "copy start run"
