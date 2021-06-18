09a9d4c94ca38c55d6294c83249b87028303f2fc
Ignacio Dominguez Martinez-Casanueva <i.dominguezm@alumnos.upm.es>
2021-06-18 12:24:00 +0200

    [S-9 UpdatePrometheus models to paper's model

--- weaver/app/semantic_tools/models/metric.py
+++ weaver/app/semantic_tools/models/metric.py
@@ -1,42 +1,23 @@
-from .ngsi_ld.entity import Entity, Property, Relationship
-from .common import Agent, URI
+from .ngsi_ld.entity import Asset, Property, Relationship
 from typing import Literal, Optional
 
 
-class Prometheus(Agent):
+class Metric(Asset):
+    type: Literal["Metric"] = "Metric"
+    labels: Property
+    hasMetricFamily: Relationship
+    hasPrometheus: Relationship
+
+
+class MetricFamily(Asset):
+    type: Literal["MetricFamily"] = "MetricFamily"
+    familyName: Property
+    familyType: Property
+    help: Optional[Property]
+    unit: Optional[Property]
+
+
+class Prometheus(Asset):
     type: Literal["Prometheus"] = "Prometheus"
     hasEndpoint: Relationship
-    name: Property
     version: Optional[Property]
-
-
-class MetricSource(Agent):
-    type: Literal["MetricSource"] = "MetricSource"
-    expression: Optional[Property] = None
-    collectsFrom: Relationship
-    interval: Property
-    name: Property
-
-
-class MetricProcessor(Agent):
-    type: Literal["MetricProcessor"] = "MetricProcessor"
-    hasInput: Relationship
-    hasApplication: Relationship
-    name: Property
-    arguments: Optional[Property] = None
-    jobId: Optional[Property]
-
-
-class MetricTarget(Agent):
-    type: Literal["MetricTarget"] = "MetricTarget"
-    hasInput: Relationship
-    uri: URI
-
-
-class StreamApplication(Agent):
-    type: Literal["StreamApplication"] = "StreamApplication"
-    fileName: Property
-    fileId: Optional[Property]
-    entryClass: Optional[Property]
-    description: Property
-    uri: URI
