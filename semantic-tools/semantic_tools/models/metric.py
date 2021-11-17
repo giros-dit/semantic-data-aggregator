36ece4069c7f28957f2c53728172931f8ade2b3e
Ignacio Dominguez Martinez-Casanueva <i.dominguezm@alumnos.upm.es>
2021-11-17 17:46:09 +0100

    [S-96] Integrate changes from S-115

--- semantic-tools/semantic_tools/models/metric.py
+++ semantic-tools/semantic_tools/models/metric.py
@@ -1,14 +1,15 @@
+from typing import Literal, Optional
+
 from semantic_tools.models.common import Asset, Infrastructure
 from semantic_tools.models.ngsi_ld.entity import Property, Relationship
-from typing import Literal, Optional
 
 
 class Metric(Asset):
     type: Literal["Metric"] = "Metric"
     hasLabels: Relationship
     hasMetricFamily: Relationship
-    hasPrometheus: Relationship
-    hasPrometheusExporter: Relationship
+    hasPrometheus: Optional[Relationship]
+    hasPrometheusExporter: Optional[Relationship]
 
 
 class MetricFamily(Asset):
