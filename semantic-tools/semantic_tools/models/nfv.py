478fd12862f801aee6024fbd960bc4a938966399
Ignacio Dominguez Martinez-Casanueva <ignacio.dominguezmartinez@telefonica.com>
2022-05-30 19:02:35 +0200

    semantic-tools: deprecate models library

--- semantic-tools/semantic_tools/models/nfv.py
+++
@@ -1,9 +0,0 @@
-from semantic_tools.models.common import Asset
-from semantic_tools.models.ngsi_ld.entity import Property, Relationship
-from typing import Literal, Optional
-
-
-class VNF(Asset):
-    type: Literal["VNF"] = "VNF"
-    descriptorId: Property
-    hasMetrics: Optional[Relationship] = None
