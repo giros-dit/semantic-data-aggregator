3ba7bffc819e9dc41dc38244c38e66c5ff96ee16
daniel-gonzalez-sanchez <daniel.gonzalez.sanchez@alumnos.upm.es>
2023-04-21 10:38:32 +0200

    Updating and tuning minor configuration things according to the SDA testbed for PALANTIR project

--- docker/crypto-detector/crypto-producer/crypto-producer.py
+++ docker/crypto-detector/crypto-producer/crypto-producer.py
@@ -35,7 +35,6 @@ def main(args):
     benign_traffic = 'ts,te,td,sa,da,sp,dp,pr,flg,fwd,stos,ipkt,ibyt,opkt,obyt,in,out,sas,das,smk,dmk,dtos,dir,nh,nhb,svln,dvln,ismc,odmc,idmc,osmc,mpls1,mpls2,mpls3,mpls4,mpls5,mpls6,mpls7,mpls8,mpls9,mpls10,cl,sl,al,ra,eng,exid,tr,$,64.95464773702646,74.23388312803024,32704.665135592822,3164.219268332289,503.5,42.625,10.335777126099709,0.875,tpkt,tbyt,cp,prtcp,prudp,pricmp,prigmp,prother,flga,flgs,flgf,flgr,flgp,flgu'
     crypto_traffic = 'ts,te,td,sa,da,sp,dp,pr,flg,fwd,stos,ipkt,ibyt,opkt,obyt,in,out,sas,das,smk,dmk,dtos,dir,nh,nhb,svln,dvln,ismc,odmc,idmc,osmc,mpls1,mpls2,mpls3,mpls4,mpls5,mpls6,mpls7,mpls8,mpls9,mpls10,cl,sl,al,ra,eng,exid,tr,$,31.94037796113921,31.94037796113921,14213.468192706949,3119.510247537929,445.0,97.66666666666669,4.5563139931740615,1.0,tpkt,tbyt,cp,prtcp,prudp,pricmp,prigmp,prother,flga,flgs,flgf,flgr,flgp,flgu'
     dcp_standard_traffic= '2023-01-17 07:02:25,2023-01-17 07:02:26,0.421,fe80::60df:46cb:1382:b255,ff02::1:3,54347,5355,UDP,......,0,0,2,158,0,0,0,0,0,0,0,0,0,0,0.0.0.0,0.0.0.0,0,0,00:00:00:00:00:00,00:00:00:00:00:00,00:00:00:00:00:00,00:00:00:00:00:00,0-0-0,0-0-0,0-0-0,0-0-0,0-0-0,0-0-0,0-0-0,0-0-0,0-0-0,0-0-0,0.000,0.000,0.000,10.101.44.1,0/0,1,2023-01-17 07:03:01.822,$,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,tpkt,tbyt,cp,prtcp,prudp,pricmp,prigmp,prother,flga,flgs,flgf,flgr,flgp,flgu'
-    bad_dcp_standard_traffic= '2023-01-17 07:02:25,2023-01-17 07:02:26,0.421,fe80::60df:46cb:1382:b255,ff02::1:3,54347,5355,UDP,......,0,0,2,158,0,0,0,0,0,0,0,0,0,0,0.0.0.0,0.0.0.0,0,0,00:00:00:00:00:00,00:00:00:00:00:00,00:00:00:00:00:00,00:00:00:00:00:00,0-0-0,0-0-0,0-0-0,0-0-0,0-0-0,0-0-0,0-0-0,0-0-0,0-0-0,0-0-0,0.000,0.000,0.000,10.101.44.1,0/0,1,2023-01-17 07:03:01.822,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,tpkt,tbyt,cp,prtcp,prudp,pricmp,prigmp,prother,flga,flgs,flgf,flgr,flgp,flgu'
 
     logging.info("Passed: -b " + KAFKA_BROKER + " -p " + KAFKA_TOPIC_PRODUCE + " -t " + TIME_BETWEEN_PRODUCE)
     logging.info("Launching producer")
@@ -73,10 +72,6 @@ def main(args):
         time.sleep(int(TIME_BETWEEN_PRODUCE))
 
         producer.publish_to_kafka(dcp_standard_traffic)
-
-        time.sleep(int(TIME_BETWEEN_PRODUCE))
-
-        producer.publish_to_kafka(bad_dcp_standard_traffic)
 
         time.sleep(int(TIME_BETWEEN_PRODUCE))
 
--- semantic-metrics-datamodels/drivers/jinja/openmetrics-reverse/README.md
+++
@@ -1,8 +0,0 @@
-# NOTE
-
-
-When testing with the sample NiFi templates, first copy the Jinja2 template in the NiFi container as shown below:
-
-```bash
-docker cp openmetrics-template.j2  nifi:/tmp/openmetrics-template.j2
-```
